4e2777bb9c3cc451a64eafccc72f1530bc10edc1
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-09-21 22:11:44 +0530

    Route setup
    All

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='oreder' element={<Orders />} />
+      </Routes>
+    </div>
   )
 }
 
